86e65a16d2e2bdcf6ac6d37bc2cc74145502ed26
Nguyễn Thành Nhân <nhanntpc06420@gmail.com>
2025-03-15 20:24:06 +0700

    Merge Khanh into Dev

--- DTOs/Request/SubjectRequest.cs
+++ DTOs/Request/SubjectRequest.cs
@@ -4,7 +4,6 @@
     {
         public int SubjectTypeId { get; set; }
         public int TeachingAssignmentId { get; set; }
-        public int SubjectGroupId { get; set; }
         public bool? IsStatus { get; set; }
         public string? SubjectCode { get; set; }
         public string? SubjectName { get; set; }
