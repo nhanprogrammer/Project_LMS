35de6ce994ae9a36a5588f6c3e0defca43a31055
quiLV <quilevan1234abc@gmail.com>
2025-03-18 08:10:59 +0700

    fix_academichold

--- DTOs/Request/UpdateAcademicHoldRequest.cs
+++ DTOs/Request/UpdateAcademicHoldRequest.cs
@@ -4,6 +4,7 @@ public class UpdateAcademicHoldRequest
 {
     public int Id { get; set; }
     public int? UserId { get; set; }
+    public int ClassId { get; set; }
     public DateTime HoldDate { get; set; }
     public int? HoldDuration { get; set; }
     public string? Reason { get; set; }
--- Services/AcademicHoldsService.cs
+++ Services/AcademicHoldsService.cs
@@ -1,4 +1,4 @@
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Project_LMS.Data;
 using Project_LMS.DTOs.Request;
@@ -26,20 +26,40 @@ namespace Project_LMS.Services
 
         public async Task<PaginatedResponse<AcademicHoldResponse>> GetPagedAcademicHolds(PaginationRequest request)
         {
-            var query = _academicHoldRepository.GetQueryable()
-                         .Where(ah => !ah.IsDelete);
-            int totalItems = await query.CountAsync();
+            var query = from ah in _academicHoldRepository.GetQueryable().Where(ah => !ah.IsDelete)
+                        join c in _context.Classes on ah.UserId equals c.UserId into classGroup
+                        from classInfo in classGroup.DefaultIfEmpty()
+                        join u in _context.Users on ah.UserId equals u.Id into userGroup
+                        from userInfo in userGroup.DefaultIfEmpty()
+                        select new AcademicHoldResponse
+                        {
+                            Id = ah.Id,
+                            UserId = ah.UserId,
+                            UserCode = userInfo != null ? userInfo.UserCode : null,
+                            FullName = userInfo != null ? userInfo.FullName : null,
+                            BirthDate = userInfo != null ? userInfo.BirthDate : null,
+                            Gender = userInfo != null ? userInfo.Gender[0] : null,
+                            ClassName = classInfo != null ? classInfo.Name : null,
+                            HoldDate = ah.HoldDate,
+                            HoldDuration = ah.HoldDuration,
+                            Reason = ah.Reason,
+                            //FileName = ah.FileName,
+                            //CreateAt = ah.CreateAt,
+                            //UserCreate = ah.UserCreate,
+                            //IsDelete = ah.IsDelete,
+                        };
 
+            int totalItems = await query.CountAsync();
             int pageSize = request.PageSize > 0 ? request.PageSize : 10;
 
-            var academicYearsList = await query
+            var academicHoldsList = await query
                 .Skip((request.PageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
 
             return new PaginatedResponse<AcademicHoldResponse>
             {
-                Items = _mapper.Map<List<AcademicHoldResponse>>(academicYearsList),
+                Items = _mapper.Map<List<AcademicHoldResponse>>(academicHoldsList),
                 PageNumber = request.PageNumber,
                 PageSize = pageSize,
                 TotalItems = totalItems,
@@ -49,30 +69,70 @@ namespace Project_LMS.Services
             };
         }
 
-        public async Task<AcademicHoldResponse> GetByIdAcademicHold(int id)
+        public async Task<User_AcademicHoldResponse> GetById(int id)
         {
-            var academicHold = await _context.AcademicHolds
+            var user = await _context.Users
                 .AsNoTracking()
-                .FirstOrDefaultAsync(ah => ah.Id == id && !ah.IsDelete);
+                .Include(u => u.StudentStatus)
+                .Include(u => u.Departments)
+                .Include(u => u.Classes)
+                .FirstOrDefaultAsync(u => u.Id == id && !(u.IsDelete ?? false));
 
-            if (academicHold == null)
-                return null;
+            if (user == null) return null;
 
-            return new AcademicHoldResponse
+            var academicYear = await _context.AcademicYears
+                .OrderByDescending(a => a.StartDate)
+                .FirstOrDefaultAsync();
+
+            return new User_AcademicHoldResponse
             {
-                Id = academicHold.Id,
-                UserId = academicHold.UserId,
-                HoldDate = academicHold.HoldDate,
-                HoldDuration = academicHold.HoldDuration,
-                Reason = academicHold.Reason,
-                FileName = academicHold.FileName,
-                CreateAt = academicHold.CreateAt,
-                UserCreate = academicHold.UserCreate
+                Id = user.Id,
+                FullName = user.FullName,
+                Gender = user.Gender,
+                BirthDate = user.BirthDate,
+                PlaceOfBirth = user.PlaceOfBirth,
+                Ethnicity = user.Ethnicity,
+                Religion = user.Religion,
+                AcademicStartDate = academicYear?.StartDate?.Year.ToString(),
+                AcademicEndDate = academicYear?.EndDate?.Year.ToString(),
+                //DepartmentName = string.Join(", ", user.Departments.Select(d => d.Name)),
+                DepartmentName = user.Departments?.FirstOrDefault()?.Name,
+                //ClassName = string.Join(", ", user.Classes.Select(d => d.Name)),
+                ClassName = user.Classes?.FirstOrDefault()?.Name,
+                UserCode = user.UserCode,
+                StartDate = user.StartDate,
+                AdmissionType = user.AdmissionType,
+                StatusName_Student = user.StudentStatus?.StatusName,
+                Address = user.Address,
+                Email = user.Email,
+                Phone = user.Phone,
+                FullnameFather = user.FullnameFather,
+                FullnameMother = user.FullnameMother,
+                FullnameGuardianship = user.FullnameGuardianship,
+                BirthFather = user.BirthFather,
+                BirthMother = user.BirthMother,
+                BirthGuardianship = user.BirthGuardianship,
+                WorkFather = user.WorkFather,
+                WorkMother = user.WorkMother,
+                WorkGuardianship = user.WorkGuardianship,
+                PhoneFather = user.PhoneFather,
+                PhoneMother = user.PhoneMother,
+                PhoneGuardianship = user.PhoneGuardianship,
             };
         }
 
         public async Task AddAcademicHold(CreateAcademicHoldRequest academicHold)
         {
+            // Lấy thông tin Class từ ClassId nếu cần
+            var classInfo = await _context.Classes
+                .Where(c => c.Id == academicHold.ClassId && !(c.IsDelete ?? false))
+                .FirstOrDefaultAsync();
+
+            if (classInfo == null)
+            {
+                throw new Exception("Class không tồn tại!");
+            }
+
             var newAcademicHold = new AcademicHold
             {
                 UserId = academicHold.UserId,
@@ -85,37 +145,46 @@ namespace Project_LMS.Services
                 IsDelete = false
             };
 
+            academicHold.UserId = newAcademicHold.UserId;
             _context.AcademicHolds.Add(newAcademicHold);
             await _context.SaveChangesAsync();
+
         }
+
 
         public async Task UpdateAcademicHold(UpdateAcademicHoldRequest academicHold)
         {
-            var existingAcademicHold = await _context.AcademicHolds.FindAsync(academicHold.Id);
-            if (existingAcademicHold != null)
-            {
-                existingAcademicHold.UserId = academicHold.UserId;
-                existingAcademicHold.HoldDate = DateTime.SpecifyKind(academicHold.HoldDate, DateTimeKind.Unspecified);
-                existingAcademicHold.HoldDuration = academicHold.HoldDuration;
-                existingAcademicHold.Reason = academicHold.Reason;
-                existingAcademicHold.FileName = academicHold.FileName;
-                existingAcademicHold.UpdateAt = DateTime.SpecifyKind(academicHold.UpdateAt, DateTimeKind.Unspecified);
-                existingAcademicHold.UserUpdate = academicHold.UserUpdate;
+            var existingHold = await _context.AcademicHolds
+                                             .Where(ah => ah.Id == academicHold.Id && !ah.IsDelete)
+                                             .FirstOrDefaultAsync();
 
-                _context.AcademicHolds.Update(existingAcademicHold);
-                await _context.SaveChangesAsync();
+            if (existingHold == null)
+            {
+                throw new Exception("Academic Hold không tồn tại!");
             }
+
+            var classInfo = await _context.Classes
+                .Where(c => c.Id == academicHold.ClassId && !(c.IsDelete ?? false))
+                .FirstOrDefaultAsync();
+
+            if (classInfo == null)
+            {
+                throw new Exception("Class không tồn tại!");
+            }
+
+            existingHold.UserId = academicHold.UserId;
+            existingHold.HoldDate = academicHold.HoldDate;
+            existingHold.HoldDuration = academicHold.HoldDuration;
+            existingHold.Reason = academicHold.Reason;
+            existingHold.FileName = academicHold.FileName;
+            existingHold.UpdateAt = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified);
+            existingHold.UserUpdate = academicHold.UserUpdate;
+
+            _context.AcademicHolds.Update(existingHold);
+            await _context.SaveChangesAsync();
+
         }
 
-        //public async Task DeleteAcademicHold(int id)
-        //{
-        //    var academicHold = await _context.AcademicHolds.FindAsync(id);
-        //    if (academicHold != null)
-        //    {
-        //        academicHold.IsDelete = true;
-        //        await _context.SaveChangesAsync();
-        //    }
-        //}
         public async Task<bool> DeleteAcademicHold(int id)
         {
             var existingAcademicYear = await _academicHoldRepository.GetByIdAsync(id);
