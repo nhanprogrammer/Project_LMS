66cca44306e5697fda8dda40d33853f5901a4023
Nguyễn Thành Nhân <nhanntpc06420@gmail.com>
2025-03-21 15:42:53 +0700

    feat(cloudinary): add delete functionality for images and files

--- Exceptions/BadRequestException.cs
+++ Exceptions/BadRequestException.cs
@@ -4,13 +4,17 @@
     {
         public List<ValidationError> Errors { get; }
 
+        public BadRequestException(string message) : base(message)
+        {
+            Errors = new List<ValidationError>();
+        }
+
         public BadRequestException(string message, List<ValidationError> errors) : base(message)
         {
             Errors = errors;
         }
 
-
-        public static BadRequestException BadRequest(string message)
+        public static BadRequestException Create(string message)
         {
             return new BadRequestException(message, new List<ValidationError>());
         }
